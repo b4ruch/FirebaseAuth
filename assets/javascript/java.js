8539e657c8c5e21fe3ef5339a20a0e303071985a
b4ruch <12469509+b4ruch@users.noreply.github.com>
2018-07-14 08:39:54 -0700

    setting up FirebaseUI 20

--- assets/javascript/java.js
+++ assets/javascript/java.js
@@ -145,7 +145,7 @@ var uiConfig = {
     signInSuccessUrl: 'index.html',
     signInOptions: [
         // Leave the lines as is for the providers you want to offer your users.
-        firebase.auth.PhoneAuthProvider.PROVIDER_ID,
+        firebase.auth.PhoneAuthProvider.PROVIDER_ID
         // firebase.auth.EmailAuthProvider.PROVIDER_ID
     ],
 
