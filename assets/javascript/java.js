967359d000d06ecf97f750da6d28b5b65315999f
b4ruch <12469509+b4ruch@users.noreply.github.com>
2018-07-14 08:39:04 -0700

    setting up FirebaseUI 19

--- assets/javascript/java.js
+++ assets/javascript/java.js
@@ -146,7 +146,7 @@ var uiConfig = {
     signInOptions: [
         // Leave the lines as is for the providers you want to offer your users.
         firebase.auth.PhoneAuthProvider.PROVIDER_ID,
-        firebase.auth.EmailAuthProvider.PROVIDER_ID
+        // firebase.auth.EmailAuthProvider.PROVIDER_ID
     ],
 
     callbacks: {
